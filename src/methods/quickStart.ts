2c3a20143251a9b08ceb50163dd71d43371a0c53
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-13 02:40:42 +0800

    fix(Android): cannot open GKD

--- src/methods/quickStart.ts
+++ src/methods/quickStart.ts
@@ -1,13 +1,30 @@
 import { LaunchApp } from "web-launch-app";
 
 export const quickStart = () => {
-  const launchApp = new LaunchApp();
-  launchApp.open({
-    scheme: 'gkd://import',
-    timeout: 2000,
+  var config = {
+    inApp: false,
+    appVersion: '',
+    pkgName: 'li.songe.gkd',
+    deeplink: {
+      scheme: {
+        android: {
+          protocol: 'gkd',
+          index: {
+            path: 'import',
+
+          },
+        },
+      },
+    },
     pkgs: {
       android: 'https://registry.npmmirror.com/@gkd-kit/app/2.0.17/files/dist/gkd-v1.7.3.apk',
     },
+    launchType: {
+      android: 'scheme',
+    },
+    timeout: 2000,
     landPage: 'https://gkd.li/guide/#install',
-  });
+  };
+  const launchApp = new LaunchApp(config);
+  launchApp.open(config);
 }
